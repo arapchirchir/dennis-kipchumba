ed4d18dbd28b5a6719fb01ca7e05197a5730589b
Mr. Toing'eny <kipchumbadennis10@gmail.com>
2022-11-28 20:02:02 +0300

    Add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
+        <p className='p-3'>
           Welcome to my profile
         </p>
-        <a href='/'>
+        <a className='text-white hover:backdrop-brightness-50 hover:text-blue-500 px-7 py-1 hover:rounded-md' href='/'>
           Dennis Kipchumba
         </a>
       </header>
