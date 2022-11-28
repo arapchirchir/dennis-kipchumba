5a8f2ade9a2e4cbaa2ac92d19c4080a094964b78
Mr. Toing'eny <kipchumbadennis10@gmail.com>
2022-11-28 20:40:37 +0300

    ASD

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App text-center bg-slate-400">
+      <header className="App-header min-h-screen flex flex-col items-center justify-center">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p className='p-3'>
-          Welcome to my profile
+        <p className='p-3 uppercase text-lg'>
+          Hire me
         </p>
-        <a className='text-white hover:backdrop-brightness-50 hover:text-blue-500 px-7 py-1 hover:rounded-md' href='/'>
+        <a className='text-white hover:backdrop-brightness-50 hover:text-blue-500 px-7 py-1 hover:rounded-md font-bold text-xl' href='/'>
           Dennis Kipchumba
         </a>
       </header>
